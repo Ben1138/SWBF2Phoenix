bc0ececa8e1addc4efb10669ac2f642ef398fca3
Ben1138 <marten.mildt@web.de>
2021-02-21 01:31:24 +0100

    Some reorganization and UI stuff.

---
+++ UnityProject/Assets/UI/UISettings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UISettings
+{
+    public const float FlickerIntensity = 0.2f;
+    public const float FlickerSpeed     = 10.0f;
+}
