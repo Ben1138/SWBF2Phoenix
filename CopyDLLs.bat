3e1e471e9309c5957ae7f1d6b0479e20d1a3c406
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-02-21 01:30:07 +0100

    Utilized quisten's shader and adapted the rings to represent a percentage. Copy .NET pdb.

--- CopyDLLs.bat
+++ CopyDLLs.bat
@@ -1,3 +1,4 @@
 copy "lua5.0-swbf2-x64\bin\x64\lua50-swbf2-x64.dll" "UnityProject\Assets\Libraries\lua50-swbf2-x64.dll" /Y
 copy "LibSWBF2\x64\Debug\LibSWBF2.dll" "UnityProject\Assets\Libraries\LibSWBF2.dll" /Y
 copy "LibSWBF2\LibSWBF2.NET\bin\x64\Debug\LibSWBF2.NET.dll" "UnityProject\Assets\Libraries\LibSWBF2.NET.dll" /Y
+copy "LibSWBF2\LibSWBF2.NET\bin\x64\Debug\LibSWBF2.NET.pdb" "UnityProject\Assets\Libraries\LibSWBF2.NET.pdb" /Y
