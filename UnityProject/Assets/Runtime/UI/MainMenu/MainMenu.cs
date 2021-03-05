d8920be28eaf98cad92a0445c13f1e463e37187c
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-03-05 14:10:28 +0100

    Introduced commandpost GameClass (unfinished).

--- UnityProject/Assets/Runtime/UI/MainMenu/MainMenu.cs
+++ UnityProject/Assets/Runtime/UI/MainMenu/MainMenu.cs
@@ -8,4 +8,14 @@ public class MainMenu : MonoBehaviour
     {
         GameRuntime.Instance.EnterMap("geo1c_con");
     }
+
+    public void StartMustafar()
+    {
+        GameRuntime.Instance.EnterMap("mus1c_con");
+    }
+
+    public void StartCorouscant()
+    {
+        GameRuntime.Instance.EnterMap("cor1c_con");
+    }
 }
