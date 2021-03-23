ed8d8389c81f1d8a77d67c0afb2b6e080c3a5b68
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-03-23 17:59:50 +0100

    Working character preview with animation in character selection menu

--- UnityProject/Assets/Runtime/UI/CharacterSelect/CharacterSelect.cs
+++ UnityProject/Assets/Runtime/UI/CharacterSelect/CharacterSelect.cs
@@ -1,48 +1,60 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using LibSWBF2.Enums;
 
-public class CharacterSelect : MonoBehaviour
+public class CharacterSelect : IMenu
 {
+    static GameRuntime GAME => GameRuntime.Instance;
     static RuntimeScene RTS => GameRuntime.GetScene();
 
     [Header("References")]
     public CharacterItem ItemPrefab;
     public RectTransform ListContents;
-
+    public Button BtnSpawn;
+        
     [Header("Settings")]
     public float ItemSpace = 5.0f;
     public float MaxItemHeight = 200f;
 
     List<CharacterItem> Items   = new List<CharacterItem>();
     ISWBFClass CurrentSelection = null;
+    List<ISWBFInstance> UnitPreviews = new List<ISWBFInstance>();
 
+
+    public override void Clear()
+    {
+        for (int i = 0; i < UnitPreviews.Count; ++i)
+        {
+            UnitPreviews[i].gameObject.SetActive(false);
+            Destroy(UnitPreviews[i]);
+        }
+        UnitPreviews.Clear();
+    }
 
     public void Add(ISWBFClass cl)
     {
         if (cl.ClassType != EEntityClassType.GameObjectClass)
         {
-            Debug.LogError($"Cannot add odf class '{cl.Name}' as item in character selection!");
+            Debug.LogError($"Cannot add odf class '{cl.Name}' as item to character selection!");
             return;
         }
+
+        // CSP = Char Select Preview
+        ISWBFInstance preview = RTS.CreateInstance(cl, cl.Name+"_CSP" + nameCounter++, Vector3.zero, Quaternion.identity, GAME.CharSelectTransform);
+        UnitPreviews.Add(preview);
 
         CharacterItem item = Instantiate(ItemPrefab, ListContents);
         item.OnClicked += () =>
         {
-            foreach (CharacterItem it in Items)
-            {
-                it.SetActive(false);
-            }
-            item.SetActive(true);
-            CurrentSelection = cl;
+            SetActive(item, cl, preview);
         };
 
         item.SetHeaderText(cl.LocalizedName);
 
         string detailText = "";
         MultiProp weapons = cl.P.Get<MultiProp>("WeaponName");
-        foreach (object[] weap in weapons)
+        foreach (object[] weap in weapons.Values)
         {
             ISWBFClass weapClass = RTS.GetClass(weap[0] as string);
             if (weapClass != null)
@@ -56,18 +68,40 @@ public class CharacterSelect : MonoBehaviour
 
         if (CurrentSelection == null)
         {
-            CurrentSelection = cl;
-            item.SetActive(true);
+            SetActive(item, cl, preview);
         }
         else
         {
             item.SetActive(false);
+            preview.gameObject.SetActive(false);
         }
 
-        RefreshItems();
+        ReCalcItemSize();
     }
 
-    void RefreshItems()
+    void SetActive(CharacterItem item, ISWBFClass cl, ISWBFInstance preview)
+    {
+        foreach (CharacterItem it in Items)
+        {
+            it.SetActive(false);
+        }
+        item.SetActive(true);
+        CurrentSelection = cl;
+
+        foreach (ISWBFInstance inst in UnitPreviews)
+        {
+            inst.gameObject.SetActive(false);
+        }
+        preview.gameObject.SetActive(true);
+
+        ISWBFAnimated animPreview = preview as ISWBFAnimated;
+        if (animPreview != null)
+        {
+            animPreview.PlayAnimation("human_rifle_stand_idle_lookaround_full");
+        }
+    }
+
+    void ReCalcItemSize()
     {
         float availableHeight = ListContents.rect.height - (ItemSpace * (Items.Count - 1));
         float itemHeight = availableHeight / Items.Count;
@@ -86,11 +120,25 @@ public class CharacterSelect : MonoBehaviour
     {
         Debug.Assert(ItemPrefab   != null);
         Debug.Assert(ListContents != null);
+        Debug.Assert(BtnSpawn     != null);
+
+        // For soem reason, we have to trigger the volume in order
+        // for it to be actually active...
+        GAME.CharSelectPPVolume.gameObject.SetActive(false);
+        GAME.CharSelectPPVolume.gameObject.SetActive(true);
+
+        BtnSpawn.onClick.AddListener(SpawnClicked);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    static int nameCounter = 0;
+    void SpawnClicked()
+    {
+        RTS.CreateInstance(CurrentSelection, "player" + nameCounter++, Camera.main.transform.position, Quaternion.identity);
     }
 }
