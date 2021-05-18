fc6a2b7536d294ebef510d2111bfc817ada9dffe
Ben1138 <marten.mildt@web.de>
2021-05-18 17:27:53 +0200

    Adjustments to recent Cra changes.

--- UnityProject/Assets/Runtime/GameClasses/soldier/PhxHumanAnimator.cs
+++ UnityProject/Assets/Runtime/GameClasses/soldier/PhxHumanAnimator.cs
@@ -24,33 +24,22 @@ public class PhxHumanAnimator : CraAnimator
     public float Forward;
     public bool Sprinting;
 
-    int StandSprintFull;
-    int StandRunForward;
-    int StandWalkForward;
-    int StandIdle;
-    int StandRunBackward;
+    int StandRunSprint;
+    int StandWalkRun;
+    int StandIdleWalk;
+    int StandIdleBackward;
     int StandReload;
-
-    float LastForward;
 
 
     public void Init()
     {
-        StandSprintFull  = AddState(0, PhxAnimationLoader.CreateState(transform, "human_1", "human_rifle_sprint_full", true));
-        StandRunForward  = AddState(0, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_runforward", true));
-        StandWalkForward = AddState(0, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_walkforward", true));
-        StandIdle        = AddState(0, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_idle_emote_full", true));
-        StandRunBackward = AddState(0, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_runbackward", true));
-        StandReload      = AddState(1, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_reload_full", false, "bone_a_spine"));
+        StandRunSprint    = AddState(0, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_runforward",      "human_1", "human_rifle_sprint_full",       true));
+        StandWalkRun      = AddState(0, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_walkforward",     "human_0", "human_rifle_stand_runforward",  true));
+        StandIdleWalk     = AddState(0, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_idle_emote_full", "human_0", "human_rifle_stand_walkforward", true));
+        StandIdleBackward = AddState(0, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_idle_emote_full", "human_0", "human_rifle_stand_runbackward", true));
+        StandReload       = AddState(1, PhxAnimationLoader.CreateState(transform, "human_0", "human_rifle_stand_reload_full",      false,    "bone_a_spine"));
 
-        ConnectStates(0, StandIdle, StandRunForward);
-        ConnectStates(0, StandIdle, StandWalkForward);
-        ConnectStates(0, StandIdle, StandRunBackward);
-        //ConnectStates(StandIdle, StandReload);
-
-        ConnectStates(0, StandWalkForward, StandRunForward);
-
-        SetState(0, StandIdle);
+        SetState(0, StandIdleWalk);
         SetState(1, CraSettings.STATE_NONE);
 
         OnStateFinished += StateFinished;
@@ -69,64 +58,32 @@ public class PhxHumanAnimator : CraAnimator
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        BlendTo(0, CraSettings.STATE_NONE, 0f);
-        if (Sprinting)
-        {
-            SetState(0, StandSprintFull);
-        }
-        else if (Forward > 0 && Forward <= 0.5f)
-        {
-            SetState(0, StandWalkForward);
-            SetPlaybackSpeed(0, StandWalkForward, Forward / 0.5f);
-        }
-        else if (Forward > 0.5f)
-        {
-            SetState(0, StandWalkForward);
-            BlendTo(0, StandRunForward, (Forward - 0.5f) / 0.5f);
-            //SetPlaybackSpeed(0, StandRunForward, Forward / 1f);
-        }
-        else if (Forward < 0f)
-        {
-            SetState(0, StandRunBackward);
-            SetPlaybackSpeed(0, StandRunBackward, -Forward / 1f);
-        }
-        else
-        {
-            SetState(0, StandIdle);
-        }
+        //if (Sprinting)
+        //{
+        //    SetState(0, StandRunSprint);
+        //}
+        //else if (Forward > 0 && Forward <= 0.5f)
+        //{
+        //    SetState(0, StandIdleWalk);
+        //    SetPlaybackSpeed(0, StandIdleWalk, Forward / 0.5f);
+        //}
+        //else if (Forward > 0.5f)
+        //{
+        //    SetState(0, StandIdleWalk);
+        //    //SetPlaybackSpeed(0, StandRunForward, Forward / 1f);
+        //}
+        //else if (Forward < 0f)
+        //{
+        //    SetState(0, StandIdleBackward);
+        //    SetPlaybackSpeed(0, StandIdleBackward, -Forward / 1f);
+        //}
+        //else
+        //{
+        //    SetState(0, StandIdleWalk);
+        //}
 
         Tick(Time.deltaTime);
-        LastForward = Forward;
     }
 }
-
-//[CustomEditor(typeof(PhxHumanAnimator))]
-//[CanEditMultipleObjects]
-//public class CraAnimatorEditor : Editor
-//{
-//    int ChosenLayer = 0;
-
-//    void OnEnable()
-//    {
-
-//    }
-
-//    public override void OnInspectorGUI()
-//    {
-//        DrawDefaultInspector();
-
-//        PhxHumanAnimator anim = (PhxHumanAnimator)target;
-
-//        ChosenLayer = EditorGUILayout.IntField(ChosenLayer);
-//        if (ChosenLayer >= 0 && ChosenLayer < anim.Layers.Length)
-//        {
-//            CraLayer layer = anim.Layers[ChosenLayer];
-//            layer.CurrentStateIdx = EditorGUILayout.IntField(layer.CurrentStateIdx);
-//            layer.BlendStateIdx = EditorGUILayout.IntField(layer.BlendStateIdx);
-//            layer.BlendValue = EditorGUILayout.FloatField(layer.BlendValue);
-//        }
-//    }
-//}
