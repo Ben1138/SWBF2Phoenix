8809e92d137c47d32efb65d8b10780baeceec249
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-06-22 23:44:27 +0200

    Added BUILD directory with package batch.

---
+++ BUILD/PackageWindows.bat
@@ -0,0 +1,2 @@
+del "Phoenix_Data\Plugins\x86_64\lib_burst_generated.txt"
+7za.exe a Phoenix.zip .\Phoenix_Data baselib.dll GameAssembly.dll Phoenix.exe UnityCrashHandler64.exe UnityPlayer.dll WinPixEventRuntime.dll
