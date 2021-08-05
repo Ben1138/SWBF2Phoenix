1c16dc4277f1f025fe4f9a4a0b75cb5e5e4f02a1
William Herald Snyder <will.herald.snyder@gmail.com>
2021-08-05 13:18:07 -0400

    SetDynamic added to PhxEffect to ensure effects attached to moving sources simulate properly

--- UnityProject/Assets/Runtime/GameClasses/effects/PhxEffect.cs
+++ UnityProject/Assets/Runtime/GameClasses/effects/PhxEffect.cs
@@ -7,7 +7,7 @@ using LibSWBF2.Utils;
 
 public class PhxEffect
 {
-    GameObject EffectObject;
+    public GameObject EffectObject;
     List<ParticleSystem> Systems;
 
     public readonly string EffectName = "";
@@ -15,6 +15,16 @@ public class PhxEffect
     public bool IsFree = true;
 
     public bool IsPlaying = false;
+
+    public bool IsStillPlaying()
+    {
+        foreach (ParticleSystem ps in Systems)
+        {
+            if (ps.isPlaying) return true;
+        }
+
+        return false;
+    }
 
 
     public PhxEffect(GameObject fx)
@@ -30,6 +40,8 @@ public class PhxEffect
             EffectObject = fx;
 
             EffectName = fx.name;
+
+            fx.SetActive(false);
         }
     }
 
@@ -47,13 +59,19 @@ public class PhxEffect
             } 
 
             EffectName = EffectToCopy.EffectObject.name; 
+
+            EffectObject.SetActive(false);
         }
     }
 
     public void SetParent(Transform Parent)
     {
         if (EffectObject != null)
+        {
             EffectObject.transform.parent = Parent;
+            EffectObject.transform.localPosition = Vector3.zero;
+            EffectObject.transform.localRotation = Quaternion.identity;             
+        }
     }
 
     public void SetLocalTransform(Vector3 Position, Quaternion Rotation)
@@ -77,6 +95,29 @@ public class PhxEffect
         {
             var mainMod = ps.main;
             mainMod.loop = Loop;
+        }
+    }
+
+    // Needs to be set if source is moving!!!
+    public void SetDynamic(bool Dynamic = true)
+    {
+        foreach (ParticleSystem ps in Systems)
+        {
+            var velLifetime = ps.velocityOverLifetime;
+
+            // If velLifetime is not enabled, we know the effect is stationary and thus should be left alone.
+            // If it is enabled, we know all simulation must be done in world space so things like missile trails
+            // and smoke plumes from damage effects will linger realistically.
+
+            // TODO: If an effect has a dummy acceleration (e.g. (0,0,0)) in its position transformation,
+            // does that mean it should simulate in world space?
+            if (velLifetime.enabled)
+            {
+                velLifetime.space = Dynamic ? ParticleSystemSimulationSpace.World : ParticleSystemSimulationSpace.Local;
+                
+                var mainModule = ps.main;
+                mainModule.simulationSpace = Dynamic ? ParticleSystemSimulationSpace.World : ParticleSystemSimulationSpace.Local;
+            }
         }
     }
 
@@ -112,7 +153,6 @@ public class PhxEffect
             ps.Stop(true, ParticleSystemStopBehavior.StopEmitting);
         }
         EffectObject?.SetActive(false);
-
     }
 
     public bool Refresh()
