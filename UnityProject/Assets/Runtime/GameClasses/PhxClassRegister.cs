775f0a5e17421093ef6d3d1d2a2d57339ac01ca4
Ben1138 <marten.mildt@web.de>
2021-08-07 21:27:38 +0200

    Able to register game classes, which either do not possess a class type (because there are no class properties), or are not instantiated by the runtime scene (e.g. projectiles, as of yet).

--- UnityProject/Assets/Runtime/GameClasses/PhxClassRegister.cs
+++ UnityProject/Assets/Runtime/GameClasses/PhxClassRegister.cs
@@ -11,11 +11,15 @@ public static class PhxClassRegister
 
         public GameBaseClass(Type ct, Type it)
         {
+            Debug.Assert(ct != null || it != null);
+
             ClassType = ct;
             InstanceType = it;
         }
     }
 
+    // When setting either the class type or instance type to null, make sure that either noone will
+    // attempt to resolve the class string to a class type, or create an instance of it respectively.
     static Dictionary<string, GameBaseClass> TypeDB = new Dictionary<string, GameBaseClass>()
     {
         { "commandpost",    new GameBaseClass(typeof(PhxCommandpost.ClassProperties),    typeof(PhxCommandpost))    },
@@ -23,7 +27,11 @@ public static class PhxClassRegister
         { "cannon",         new GameBaseClass(typeof(PhxCannon.ClassProperties),         typeof(PhxCannon))         },
         { "grenade",        new GameBaseClass(typeof(PhxGrenade.ClassProperties),        typeof(PhxGrenade))        },
         { "powerupstation", new GameBaseClass(typeof(PhxPowerupstation.ClassProperties), typeof(PhxPowerupstation)) },
-        { "bolt",           new GameBaseClass(typeof(PhxBolt), typeof(PhxBolt)) },
+
+        // Right now, there's custom object pooling just for projectiles, meaning, PhxBolt's are not instantiated via
+        // PhxRuntimeScene.CreateInstance(), but with PhxProjectiles.FireProjectile()
+        // Maybe this will be obsolete once we've got a generic object pooling for everything. Idk yet.
+        { "bolt",           new GameBaseClass(typeof(PhxBolt),                           null)                      },
     };
 
     public static Type GetPhxInstanceType(string name)
