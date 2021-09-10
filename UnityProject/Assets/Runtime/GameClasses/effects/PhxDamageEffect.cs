406a9ee37d8c0b984cf08cf62383de31e42feca5
William Herald Snyder <will.herald.snyder@gmail.com>
2021-09-10 14:54:18 -0400

    PhxDamageEffect minor updates

--- UnityProject/Assets/Runtime/GameClasses/effects/PhxDamageEffect.cs
+++ UnityProject/Assets/Runtime/GameClasses/effects/PhxDamageEffect.cs
@@ -22,24 +22,29 @@ public class PhxDamageEffect
     		Init();
     	}
 
-    	if (healthPercent <= DamageStartPercent && healthPercent > DamageStopPercent)
-    	{
-    		if (!Effect.IsPlaying)
-    		{
-    			Effect.Play();
-    		}  
+        if (Effect != null)
+        {
+        	if (healthPercent <= DamageStartPercent && healthPercent > DamageStopPercent)
+        	{
+        		if (!Effect.IsPlaying)
+        		{
+        			//Debug.LogFormat("Playing damage effect {0}", Effect.EffectObject.name);
+        			Effect.Play();
+        		}  
 
-    		IsOn = true;  		
-    	}
-    	else 
-    	{
-    		if (Effect.IsPlaying)
-    		{
-    			Effect.Stop();
-    		}
+        		IsOn = true;  		
+        	}
+        	else 
+        	{
+        		if (Effect.IsPlaying)
+        		{
+                    //Debug.LogFormat("Stopping damage effect {0}", Effect.EffectObject.name);
+        			Effect.Stop();
+        		}
 
-    		IsOn = false;
-    	}
+        		IsOn = false;
+        	}
+        }
     }
 
 
@@ -50,6 +55,7 @@ public class PhxDamageEffect
     		Effect.SetParent(DamageAttachPoint);
     		Effect.SetLocalTransform(Vector3.zero, Quaternion.identity);
     		Effect.SetLooping(true);
+            Effect.SetDynamic(true);
     		Effect.Stop();
     	}
 
