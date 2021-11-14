17a4cac1f9ef71f36957d899491204383ce10bdc
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2021-11-14 18:54:31 +0100

    Added batch script to build on windows

--- CopyDLLs.bat
+++
@@ -1,4 +0,0 @@
-copy "lua5.0-swbf2-x64\bin\x64\lua50-swbf2-x64.dll" "UnityProject\Assets\Lib\lua50-swbf2-x64.dll" /Y
-copy "LibSWBF2\x64\Debug\LibSWBF2.dll" "UnityProject\Assets\Lib\LibSWBF2.dll" /Y
-copy "LibSWBF2\LibSWBF2.NET\bin\x64\Debug\LibSWBF2.NET.dll" "UnityProject\Assets\Lib\LibSWBF2.NET.dll" /Y
-copy "LibSWBF2\LibSWBF2.NET\bin\x64\Debug\LibSWBF2.NET.pdb" "UnityProject\Assets\Lib\LibSWBF2.NET.pdb" /Y
