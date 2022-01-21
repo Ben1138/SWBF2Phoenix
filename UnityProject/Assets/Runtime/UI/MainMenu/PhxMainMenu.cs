0e3156d22d0e28d8886aafb3f6f200299857a4aa
Ben1138 <8263806+Ben1138@users.noreply.github.com>
2022-01-21 19:49:39 +0100

    Introduced versioning (major.minor.patch), starting with 0.1.0. Exposing version to Lua.

--- UnityProject/Assets/Runtime/UI/MainMenu/PhxMainMenu.cs
+++ UnityProject/Assets/Runtime/UI/MainMenu/PhxMainMenu.cs
@@ -135,7 +135,7 @@ public class PhxMainMenu : PhxMenuInterface
                 item.SetIcon(modeSub.Icon);
                 item.SetIcon2(eraSub.Icon);
 
-                string mapScript = PhxSWBFHelpers.Format(mapluafile, eraSub.Sub, modeSub.Sub);
+                string mapScript = PhxHelpers.Format(mapluafile, eraSub.Sub, modeSub.Sub);
                 RotationLuaFiles.Add(mapScript);
             }
         }
